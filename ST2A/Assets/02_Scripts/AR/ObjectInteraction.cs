eabb78bc2fda68d02c37adda30beeecacd1a7ae5
je-gi <jael.gangl@gmail.com>
2024-10-20 20:51:41 +0200

    fixed script ObjectInteraction to rotate object

--- ST2A/Assets/02_Scripts/AR/ObjectInteraction.cs
+++ ST2A/Assets/02_Scripts/AR/ObjectInteraction.cs
@@ -6,36 +6,13 @@ public class ObjectInteraction : MonoBehaviour
 {
     private float initialDistance;
     private Vector3 initialScale;
-    private Quaternion initialRotation;
-    private Vector2 initialTouchPosition;
+
+    private float rotationSpeed = 0.2f;
+    private float initialRotationFingerPosX;
 
     private void Update()
     {
-        if (Input.touchCount == 1)
-        {
-            Touch touch = Input.GetTouch(0);
-
-            if (touch.phase == TouchPhase.Began)
-            {
-                initialTouchPosition = touch.position;
-                initialRotation = transform.rotation; 
-            }
-
-            if (touch.phase == TouchPhase.Moved)
-            {
-                Vector2 touchDelta = touch.position - initialTouchPosition;
-                float rotationFactor = touchDelta.x * 0.1f;
-
-              
-                transform.rotation = initialRotation * Quaternion.Euler(0, -rotationFactor, 0);
-            }
-            else if (touch.phase == TouchPhase.Ended)
-            {
-               
-                initialRotation = transform.rotation;
-            }
-        }
-        else if (Input.touchCount == 2)
+        if (Input.touchCount == 2)
         {
             Touch touch1 = Input.GetTouch(0);
             Touch touch2 = Input.GetTouch(1);
@@ -53,6 +30,27 @@ public class ObjectInteraction : MonoBehaviour
 
                 float scaleFactor = currentDistance / initialDistance;
                 transform.localScale = initialScale * scaleFactor;
+            }
+        }
+        else if (Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                initialRotationFingerPosX = touch.position.x;
+            }
+            else if (touch.phase == TouchPhase.Moved)
+            {
+                float deltaX = touch.position.x - initialRotationFingerPosX;
+
+                Vector3 currentRotation = transform.eulerAngles;
+
+                float newRotationY = currentRotation.y - deltaX * rotationSpeed;
+
+                transform.eulerAngles = new Vector3(currentRotation.x, newRotationY, currentRotation.z);
+
+                initialRotationFingerPosX = touch.position.x;
             }
         }
     }
