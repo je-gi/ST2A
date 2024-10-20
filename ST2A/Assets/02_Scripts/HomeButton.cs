a09b8c0b484386c214676ffdca783005df0fa1fd
je-gi <jael.gangl@gmail.com>
2024-10-20 22:43:33 +0200

    added music to ar scenes, sound to buttons and unified the home button

--- ST2A/Assets/02_Scripts/HomeButton.cs
+++ ST2A/Assets/02_Scripts/HomeButton.cs
@@ -1,14 +1,30 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class HomeButton : MonoBehaviour
 {
-        public void LoadHomeScene()
-    {
-      SceneManager.LoadScene("00_StartScene");
+    public AudioSource buttonSound;
     
+  
+    public void LoadHomeScene()
+    {
+        StartCoroutine(PlaySoundAndLoadScene());
+    }
+
+
+    private IEnumerator PlaySoundAndLoadScene()
+    {
+        if (buttonSound != null) 
+        {
+            buttonSound.Play();
+            
+            
+            yield return new WaitForSeconds(buttonSound.clip.length);
+        }
+        
+       
+        SceneManager.LoadScene("00_StartScene");
     }
 }
--- ST2A/Assets/Scene0_private/MainMenu.cs
+++ ST2A/Assets/Scene0_private/MainMenu.cs
@@ -4,17 +4,30 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioSource buttonSound; 
+
     public void Play()
     {
-        StartCoroutine(LoadNextScene());
+        StartCoroutine(PlaySoundAndLoadNextScene());
     }
 
-    private IEnumerator LoadNextScene()
+    private IEnumerator PlaySoundAndLoadNextScene()
     {
-        yield return new WaitForSeconds(1f);
+        
+        if (buttonSound != null)
+        {
+            buttonSound.Play(); 
+            yield return new WaitForSeconds(buttonSound.clip.length); 
+        }
+        else
+        {
+           
+            yield return new WaitForSeconds(1f);
+        }
 
         ARSceneSwitcher arSceneSwitcher = FindObjectOfType<ARSceneSwitcher>();
 
+   
         if (arSceneSwitcher != null)
         {
             arSceneSwitcher.SwitchToNextScene();
